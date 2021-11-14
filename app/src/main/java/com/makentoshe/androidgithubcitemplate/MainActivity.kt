590b40bdf95f1423dd91ec679e232c176c189721
Ivan <testestrrr@gmail.com>
2021-11-14 23:25:15 +0300

    Buttons and gradle versions

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -2,11 +2,17 @@ package com.makentoshe.androidgithubcitemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button2)
+        button.setOnClickListener {
+            println("Click on the button ")
+        }
     }
 }
